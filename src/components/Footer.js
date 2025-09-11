a4ed87cd1f74987d895f034f655ae4e4fbccb843
sathwi_pedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-09-11 21:44:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,15 @@ const Footer = ({ theme }) => {
   const footerStyle = {
     backgroundColor: isDark ? '#111' : 'rgb(47, 79, 79)',
     color: '#fff',
-    padding: '0.2rem',
+    padding: '0.5rem',
     textAlign: 'center',
     marginTop: 'auto',
+    fontSize: '0.9rem',
   };
 
   return (
     <footer style={footerStyle}>
-      <p>&copy; {new Date().getFullYear()}Developed by Sathwik Pedapati</p>
+      <p>Thank you for visiting! &copy; {new Date().getFullYear()} Developed by Sathwik Pedapati</p>
     </footer>
   );
 };
