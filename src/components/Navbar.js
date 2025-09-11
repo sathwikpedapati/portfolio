a0eab435ececac86e9028f57918af19fcfe9d04f
sathwi_pedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-09-11 23:12:28 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
       >
 <span className="navbar-brand mb-0 h1">
   <strong>S</strong>
-  <span>athwik's Space🌐</span>
+  <span style={{ color: '#fff' }}>athwik's Space🌐</span>
 </span>
         <button
           onClick={toggleTheme}
